f80ef5a7ee3562e9a46089e7f197225f61c4daf9
Viktoria Czaran <vczaran@gmail.com>
2023-05-16 12:15:40 -0400

    first round

---
+++ memory game/card.rb
@@ -0,0 +1,42 @@
+class Card
+ 
+    def initialize(face_value, status)
+      @face_value = face_value
+      @status = status
+    end
+
+    def face_up?
+     @status == "face_up"
+    end
+
+    def face_value
+      if self.face_up?
+        return @face_value
+        true
+      else
+        return false
+      end
+    end
+
+    def hide
+      @status = "face_down"
+    end
+
+
+    def reveal
+        @status = "face_up"
+    end
+
+    def ==(card)
+       card.reveal
+       self.reveal
+      unless card.face_value == self.face_value
+        card.hide 
+        self.hide
+      end
+    end
+
+
+
+
+end
