2ac02f0ce0b9176d56bdec8d14e20ef7f85f64f2
Viktoria Czaran <vczaran@gmail.com>
2023-05-16 17:00:24 -0400

    IT WORKS

--- memory game/game.rb
+++ memory game/game.rb
@@ -5,7 +5,7 @@ require_relative "human_player"
 require "byebug"
 class Game
   
-    def initialize( previous_guess = [])
+    def initialize( previous_guess = nil)
         @game_board = Board.new
         @previous_guess = previous_guess
     end
@@ -19,24 +19,28 @@ class Game
         guessed_pos = HumanPlayer.get_input
         make_guess(guessed_pos)
        end
-        
+        puts "congrats! you win :)"
     end
 
     def make_guess(pos)
         row = pos[0]
         col = pos[1]
         card = @game_board.grid[row][col]
+
         unless card.face_up?
             card.reveal
             puts "your card is #{card.face_value}"
-            if @previous_guess == []
+            if @previous_guess == nil
                 @previous_guess = card
             else
-                unless card == @previous_guess
+                if card.face_value != @previous_guess.face_value
                     @previous_guess.hide
                     card.hide
+                else
+                    @previous_guess.reveal
+                    card.reveal
                 end
-                @previous_guess = []
+                @previous_guess = nil
             end
         end
     end
